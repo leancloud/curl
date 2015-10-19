3ad1f0f98aa1b034b76c149c6465093551cb57ba
Daniel Stenberg <daniel@haxx.se>
2015-10-19 14:26:11 +0200

    http2: send new prio/deps
    When about to send off H2 frames, check if there's a prio/dep update to
    send and then put those in the ougoing queue. Also include correct info
    in initial submit request call.

--- lib/urldata.h
+++ lib/urldata.h
@@ -1337,6 +1337,10 @@ struct UrlState {
 
   curl_read_callback fread_func; /* read callback/function */
   void *in;                      /* CURLOPT_READDATA */
+
+  struct SessionHandle *stream_depends_on;
+  bool stream_depends_e; /* set or don't set the Exclusive bit */
+  int stream_prio;
 };
 
 
